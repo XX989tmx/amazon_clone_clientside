fdc5e8585724c1c8abfe14600a5822fb59f31a2a
XX989tmx <issacgravity8@gmail.com>
2020-11-04 14:45:52 +0900

    basic route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import React from "react";
 import "./App.css";
 
 function App() {
+  let routes;
+  routes = (
+    <Switch>
+      <Route></Route>
+    </Switch>
+  );
+
   return (
     <div>
-      <h1>app</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Router>{routes}</Router>
     </div>
   );
 }
