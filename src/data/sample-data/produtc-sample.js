90c8bb2c7cbd6f650feaee0cab39b29f2ed95505
XX989tmx <issacgravity8@gmail.com>
2020-11-05 07:56:12 +0900

    sample data

---
+++ src/data/sample-data/produtc-sample.js
@@ -0,0 +1,16 @@
+const product = {
+  name: "ヨシカワ ホットサンドメーカー",
+  price: {
+    beforeTax: 1473,
+    consumerTax: 100,
+    afterTax: 1100,
+    currency:"JPY"
+  },
+  deliveryDate: "11/20 2020",
+  brand:"ヨシカワ(Yoshikawa)",
+  parentCategory: ["鍋・フライパン"],
+  ancestorCategories: ["鍋・フライパン", "キッチン用品", "ホーム＆キッチン"],
+  categories:["直火式ホットサンド・ワッフルメーカー"],
+  stockQuantity: 16,
+  isStock: true,
+};
