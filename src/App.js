7953ae2148a12593402f2e16e473badacc8e446a
XX989tmx <issacgravity8@gmail.com>
2020-11-04 14:51:15 +0900

    adds home page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,34 @@ import {
 } from "react-router-dom";
 import React from "react";
 import "./App.css";
+import Home from "./static/pages/Home";
 
 function App() {
   let routes;
   routes = (
     <Switch>
-      <Route></Route>
+      <Route path="/home">
+        <Home />
+      </Route>
     </Switch>
   );
 
   return (
     <div>
-      <Router>{routes}</Router>
+      <Router>
+      {/* temporary header */}
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={"/home"}>Home Page</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        {/* main */}
+        <main> {routes}</main>
+      </Router>
     </div>
   );
 }
