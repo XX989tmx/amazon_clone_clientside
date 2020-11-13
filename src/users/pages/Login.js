6cdbdcf24a2f6a41367eea6115f84675549b00b6
XX989tmx <issacgravity8@gmail.com>
2020-11-14 05:06:03 +0900

    adds login

--- src/users/pages/Login.js
+++ src/users/pages/Login.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import Axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../../shared/context/auth-context";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const auth = useContext(AuthContext);
+  const history = useHistory();
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const loginSubmitHandler = async (event) => {
+    event.preventDefault();
+    let response;
+    let body;
+    body = {
+      email: Email,
+      password: Password,
+    };
+    try {
+      response = await Axios.post(
+        "http://localhost:8080/api/users/login",
+        body
+      );
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log(response);
+    auth.login(response.data.userId, response.data.token);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div>
+      <form action="" onSubmit={loginSubmitHandler}>
+        <input
+          id="email"
+          type="email"
+          value={Email}
+          onChange={emailChangeHandler}
+        />
+        <input
+          id="password"
+          value={Password}
+          type="password"
+          onChange={passwordChangeHandler}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
