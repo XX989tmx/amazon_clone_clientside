a4bd663c8c5cdbf2da77b90eaaa56ac87e456413
XX989tmx <issacgravity8@gmail.com>
2020-11-14 00:14:29 +0900

    adds axios

--- src/products/pages/SpecificProduct.js
+++ src/products/pages/SpecificProduct.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
 
 const SpecificProduct = () => {
-    return (
-      <div>
-        <h1>SpecificProduct</h1>
-      </div>
-    );
-}
+  const productId = useParams().productId;
+  useEffect(() => {
+    async function fetch(params) {
+      let response;
+      try {
+        response = await Axios.get(
+          `${process.env.REACT_APP_BACKEND_URL}/products/getSpecificProductById/${productId}`
+        );
+      } catch (error) {
+        console.log(error);
+      }
+      console.log(response);
+    }
+    fetch();
+  }, []);
+  return (
+    <div>
+      <h1>SpecificProduct</h1>
+    </div>
+  );
+};
 
 export default SpecificProduct;
