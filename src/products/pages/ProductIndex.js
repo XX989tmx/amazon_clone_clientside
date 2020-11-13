23eb4bfbeadeea4361913ecdefacdb595e1c9382
XX989tmx <issacgravity8@gmail.com>
2020-11-14 04:31:13 +0900

    adds sample get request

--- src/products/pages/ProductIndex.js
+++ src/products/pages/ProductIndex.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const ProductIndex = () => {
-    return (
-        <div>
-            <h1>product index</h1>
-        </div>
-    );
-}
+  useEffect(() => {
+    async function fetch(params) {
+        let response;
+        try {
+            response = await Axios.get(
+              "http://localhost:8080/api/products/productIndex/ミネラルウォーター"
+            );
+        } catch (error) {
+            console.log(error);
+        };
+        console.log(response);
+        const data = response.data;
+        console.log(data);
+    }
+    fetch();
+  }, []);
+  return (
+    <div>
+      <h1>product index</h1>
+    </div>
+  );
+};
 
 export default ProductIndex;
