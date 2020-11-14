f01c60f185d5f73d4a734e322ff2c38870d1f5e5
XX989tmx <issacgravity8@gmail.com>
2020-11-14 23:02:07 +0900

    some change

--- src/shared/components/navigation/SideNavigation.js
+++ src/shared/components/navigation/SideNavigation.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 const SideNavigation = () => {
+    // const category1 = [
+    //     {category1:"家電、カメラ、AV機器"}
+    // ];
+    // const category2 = [
+    //   {
+    //     category2: "キッチン家電",
+    //     link: `/product/productIndex/${キッチン家電}`,
+    //   },
+    //   {
+    //     category2: "生活家電",
+    //     link: `/product/productIndex/${生活家電}`,
+    //   },
+    // ];
+    
     return (
         <div>
             
