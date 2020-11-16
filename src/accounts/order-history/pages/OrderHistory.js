71d4b0de3b772eb82176d3e4d815f4fca16d9cbc
XX989tmx <issacgravity8@gmail.com>
2020-11-17 03:21:29 +0900

    adds request handler to order history js

--- src/accounts/order-history/pages/OrderHistory.js
+++ src/accounts/order-history/pages/OrderHistory.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import Axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../shared/context/auth-context";
 
 const OrderHistory = () => {
-    return (
-      <div>
-        <h1>OrderHistory</h1>
-      </div>
-    );
-}
+  const auth = useContext(AuthContext);
+  const [Message, setMessage] = useState();
+  useEffect(() => {
+    const fetch = async (params) => {
+      let response;
+      try {
+        response = await Axios.get(
+          `http://localhost:8080/api/orders/getAllOrderHistory/${auth.userId}?token=${auth.token}`
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetch();
+  }, []);
+  return (
+    <div>
+      <h1>OrderHistory</h1>
+    </div>
+  );
+};
 
 export default OrderHistory;
