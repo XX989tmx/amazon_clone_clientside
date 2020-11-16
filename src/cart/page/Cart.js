80e5f6af03c1e6908db841dff62a805eed57b517
XX989tmx <issacgravity8@gmail.com>
2020-11-17 04:41:42 +0900

    adds fetch request to cart page

--- src/cart/page/Cart.js
+++ src/cart/page/Cart.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import Axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../../shared/context/auth-context";
 
 const Cart = () => {
-    return (
-      <div>
-        <h1>Cart page</h1>
-      </div>
-    );
-}
+  // useeffectで現在のカートの中身をFetch
+  const auth = useContext(AuthContext);
+  const history = useHistory();
+  useEffect(() => {
+    async function fetch(params) {
+      let response;
+      try {
+        response = await Axios.get(
+          `http://localhost:8080/api/users/getLatestContentOfCart/${auth.userId}?token=${auth.token}`
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetch();
+  }, []);
+  // 後で買うリストデータもuseeffectでFetch。backendにロジック追加
+
+  const goToCheckoutHandler = (event) => {
+    event.preventDefault();
+    history.push("/order/checkout");
+  };
+  return (
+    <div>
+      <h1>Cart page</h1>
+
+      {/* checkout pageへ移動 */}
+      <button onClick={goToCheckoutHandler}>checkout</button>
+      <button>empty cart</button>
+    </div>
+  );
+};
 
 export default Cart;
