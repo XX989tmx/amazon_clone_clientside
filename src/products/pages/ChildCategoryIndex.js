8caad5b6e9e90049c7f051911a75283a9f282168
XX989tmx <issacgravity8@gmail.com>
2020-11-19 06:16:09 +0900

    adds http request to grand grand child index js

--- src/products/pages/ChildCategoryIndex.js
+++ src/products/pages/ChildCategoryIndex.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import Axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { AuthContext } from "../../shared/context/auth-context";
 
 const ChildCategoryIndex = () => {
-    return (
-        <div>
-            child category index 
-        </div>
-    );
-}
+  
+
+  return <div>child category index</div>;
+};
 
 export default ChildCategoryIndex;
--- src/products/pages/GrandGrandChildCategoryIndex.js
+++ src/products/pages/GrandGrandChildCategoryIndex.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { AuthContext } from '../../shared/context/auth-context';
 
 const GrandGrandChildCategoryIndex = () => {
-    return (
-        <div>
-            grand grand child category index
-        </div>
-    );
+  // grandGrandChildCategory("categories":grandGrandChildCategory)に一致するproduct documentを配列の形で受け取る。pagination16 grandGrandChildCategoryIdをクエリにして、ドキュメントを検索。
+  const grandGrandChildCategory = useParams().grandGrandChildCategory;
+  console.log(grandGrandChildCategory);
+  useEffect(() => {
+    async function getGrandGrandChildCategoryMatchedProducts(params) {
+      let response;
+      try {
+        response = await Axios.get(
+          process.env.REACT_APP_BACKEND_URL +
+            `/products/productIndex/${grandGrandChildCategory}`
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getGrandGrandChildCategoryMatchedProducts();
+  }, []);
+  return <div>grand grand child category index</div>;
 }
 
 export default GrandGrandChildCategoryIndex;
