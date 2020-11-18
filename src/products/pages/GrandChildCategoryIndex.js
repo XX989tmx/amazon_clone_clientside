258a1bbc97f48dfb90e8aa18295aae85496b043e
XX989tmx <issacgravity8@gmail.com>
2020-11-19 06:26:08 +0900

    adds http request to grand child category js

--- src/products/pages/GrandChildCategoryIndex.js
+++ src/products/pages/GrandChildCategoryIndex.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import Axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../../shared/context/auth-context";
 
 const GrandChildCategoryIndex = () => {
-    return (
-        <div>
-            grand child category index
-        </div>
-    );
-}
+  // grandChildCategory(スキーマ上は'parent categories')にマッチした配列を受け取る
+  const grandChildCategory = useParams().grandChildCategory;
+  console.log(grandChildCategory);
+  useEffect(() => {
+    async function getGrandChildCategoryMatchedProducts(params) {
+      let response;
+      try {
+        response = await Axios.get(
+          process.env.REACT_APP_BACKEND_URL +
+            `/products/getProductIndexByParentCategory/${grandChildCategory}`
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getGrandChildCategoryMatchedProducts();
+  }, []);
+
+  return <div>grand child category index</div>;
+};
 
 export default GrandChildCategoryIndex;
