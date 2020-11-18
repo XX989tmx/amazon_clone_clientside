b7014b174b5da44d551456111c65d30550a903e5
XX989tmx <issacgravity8@gmail.com>
2020-11-19 06:43:45 +0900

    adds http request to parent category js

--- src/products/pages/ParentCategoryIndex.js
+++ src/products/pages/ParentCategoryIndex.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import Axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ParentCategoryIndex = () => {
-    return (
-        <div>
-            parent category
-        </div>
-    );
-}
+  const parentCategory = useParams().parentCategory;
+  console.log(parentCategory);
+  useEffect(() => {
+    async function getParentCategoryMatchedProducts(params) {
+      let response;
+      try {
+        response = await Axios.get(
+          process.env.REACT_APP_BACKEND_URL +
+            `/products/getProductIndexByAncestorCategory/${parentCategory}`
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getParentCategoryMatchedProducts();
+  }, []);
+  return <div>parent category</div>;
+};
 
 export default ParentCategoryIndex;
