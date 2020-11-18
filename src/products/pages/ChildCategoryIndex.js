d42e62eda64bb29041f14077a925653c93e16be5
XX989tmx <issacgravity8@gmail.com>
2020-11-19 06:36:34 +0900

    adds http request to child category js

--- src/products/pages/ChildCategoryIndex.js
+++ src/products/pages/ChildCategoryIndex.js
@@ -4,7 +4,23 @@ import { useHistory, useParams } from "react-router-dom";
 import { AuthContext } from "../../shared/context/auth-context";
 
 const ChildCategoryIndex = () => {
-  
+  const childCategory = useParams().childCategory;
+  console.log(childCategory);
+  useEffect(() => {
+    async function getChildCategoryMatchedProducts(params) {
+      let response;
+      try {
+        response = await Axios.get(
+          process.env.REACT_APP_BACKEND_URL +
+            `/products/getProductIndexByAncestorCategory/${childCategory}`
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getChildCategoryMatchedProducts();
+  }, []);
 
   return <div>child category index</div>;
 };
