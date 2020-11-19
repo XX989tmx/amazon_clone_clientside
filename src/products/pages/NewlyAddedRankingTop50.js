e6a6a969ae0e11ab23f5a5c94d77f9478f1db801
XX989tmx <issacgravity8@gmail.com>
2020-11-20 05:18:00 +0900

    adds comment

--- src/products/pages/NewlyAddedRankingTop50.js
+++ src/products/pages/NewlyAddedRankingTop50.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const NewlyAddedRankingTop50 = () => {
-    return (
-      <div>
-        <h1>NewlyAddedRankingTop50</h1>
-      </div>
-    );
-}
+  //  backend logic:
+  // await Product.find({categories:categories}).sort({dateAdded??: -1}).limit(50);
+  return (
+    <div>
+      <h1>NewlyAddedRankingTop50</h1>
+    </div>
+  );
+};
 
 export default NewlyAddedRankingTop50;
--- src/products/pages/WishlistrankingTop50.js
+++ src/products/pages/WishlistrankingTop50.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const WishlistrankingTop50 = () => {
-    return <div>WishlistrankingTop50</div>;
+  //Backend:  totalNumberOdTimesOfAddedToWishlist filedを新たに追加し、ウィッシュリストに追加されるたびに＋１する。各カテゴリーレベルごとに、このフィールドが多い順ソートし、先頭５０個を受け取る。
+  // await Product.find({categories:categories}).sort({totalNumberOdTimesOfAddedToWishlist: -1}).limit(50);
+  return <div>WishlistrankingTop50</div>;
 }
 
 export default WishlistrankingTop50;
