47ab44dd3166e4429541f303c5aa2a282217459f
XX989tmx <issacgravity8@gmail.com>
2020-11-23 14:02:03 +0900

    adds structure 8

--- src/products/pages/SpecificProduct.js
+++ src/products/pages/SpecificProduct.js
@@ -906,6 +906,39 @@ const SpecificProduct = () => {
                 oshirase
               </Col>
             </Row>
+            <Row>
+              <Col xs={12} md={3} lg={3}>
+                <Row>
+                  <Col>カスタマーレビュー</Col>
+                </Row>
+                <Row>
+                  <Col>star</Col>
+                  <Col>星N中N</Col>
+                </Row>
+                <Row>
+                  <Col>N件の評価</Col>
+                </Row>
+                <Row>
+                  <Col style={{ height: "300px" }}>review stats</Col>
+                </Row>
+                <Row>
+                  <Col>この商品をレビュー</Col>
+                </Row>
+                <Row>
+                  <Col>他のお客様にも意見を伝えましょう</Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Button>カスタマーレビューを書く</Button>{" "}
+                  </Col>
+                </Row>
+              </Col>
+              <Col xs={12} md={9} lg={9}>
+                <Row>
+                  <Col xs={12}>レビュー一覧</Col>
+                </Row>
+              </Col>
+            </Row>
           </Col>
           <Col md={2} lg={2} xl={2}>
             cart side bar
