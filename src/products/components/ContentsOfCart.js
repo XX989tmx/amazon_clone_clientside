17d498d5acd17b3ffeba111c9c122238db6005ac
XX989tmx <issacgravity8@gmail.com>
2020-11-24 22:31:35 +0900

    adds structure

--- src/products/components/ContentsOfCart.js
+++ src/products/components/ContentsOfCart.js
@@ -3,14 +3,30 @@ import reactBootstrap, { Row, Col, Container, Button } from "react-bootstrap";
 const ContentsOfCart = (props) => {
   return (
     <Row>
-      rendering contents of cart{" "}
-      <Col>
-        {props.CartItems.map((v, i) => (
+      <Col style={{ height: "100%", padding: "10px 25px 10px 25px" }}>
+        <Row>
+          <Col style={{ height: "100px" }}>image</Col>
+        </Row>
+        <Row>
+          <Col>name</Col>
+        </Row>
+        <Row>
+          <Col>price</Col>
+        </Row>
+        <Row>
+          <Col md={12} lg={6}>
+            <select>
+              <option>数量:1</option>{" "}
+            </select>
+          </Col>
+          <Col md={12} lg={6}>delete</Col>
+        </Row>
+        {/* {props.CartItems.map((v, i) => (
           <Row>
             {" "}
             <Col> {v.productId.name}</Col> <Col>{v.productId.price}</Col>
           </Row>
-        ))}
+        ))} */}
       </Col>
     </Row>
   );
