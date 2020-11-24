533be4c80e6fff6695db8ac9beab0f6dc700d8af
XX989tmx <issacgravity8@gmail.com>
2020-11-24 17:30:09 +0900

    componentize grand grand child category page 2

---
+++ src/products/components/RecommendationFromPurchaseHistory.js
@@ -0,0 +1,156 @@
+import React from "react";
+import reactBootstrap, { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const RecommendationFromPurchaseHistory = (props) => {
+  return (
+    <Row>
+      <Col xs={12}>
+        <Row>
+          <Col lg={5}>
+            <h2>あなたのお買い物傾向から</h2>
+          </Col>
+          <Col lg={7}>
+            <Link>もっと見る</Link>
+          </Col>{" "}
+        </Row>
+        <Row>
+          <Col
+            xs={6}
+            md={4}
+            lg={2}
+            style={{
+              height: "100%",
+              padding: "10px 25px 10px 25px",
+            }}
+          >
+            <Row style={{ height: "250px" }}>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>price</Col>
+            </Row>
+            <Row>
+              <Col>name</Col>
+            </Row>
+            <Row>
+              <Col>review</Col>
+            </Row>
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            lg={2}
+            style={{
+              height: "100%",
+              padding: "10px 25px 10px 25px",
+            }}
+          >
+            <Row style={{ height: "250px" }}>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>price</Col>
+            </Row>
+            <Row>
+              <Col>name</Col>
+            </Row>
+            <Row>
+              <Col>review</Col>
+            </Row>
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            lg={2}
+            style={{
+              height: "100%",
+              padding: "10px 25px 10px 25px",
+            }}
+          >
+            <Row style={{ height: "250px" }}>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>price</Col>
+            </Row>
+            <Row>
+              <Col>name</Col>
+            </Row>
+            <Row>
+              <Col>review</Col>
+            </Row>
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            lg={2}
+            style={{
+              height: "100%",
+              padding: "10px 25px 10px 25px",
+            }}
+          >
+            <Row style={{ height: "250px" }}>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>price</Col>
+            </Row>
+            <Row>
+              <Col>name</Col>
+            </Row>
+            <Row>
+              <Col>review</Col>
+            </Row>
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            lg={2}
+            style={{
+              height: "100%",
+              padding: "10px 25px 10px 25px",
+            }}
+          >
+            <Row style={{ height: "250px" }}>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>price</Col>
+            </Row>
+            <Row>
+              <Col>name</Col>
+            </Row>
+            <Row>
+              <Col>review</Col>
+            </Row>
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            lg={2}
+            style={{
+              height: "100%",
+              padding: "10px 25px 10px 25px",
+            }}
+          >
+            <Row style={{ height: "250px" }}>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>price</Col>
+            </Row>
+            <Row>
+              <Col>name</Col>
+            </Row>
+            <Row>
+              <Col>review</Col>
+            </Row>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default RecommendationFromPurchaseHistory;
