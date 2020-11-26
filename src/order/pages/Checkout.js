46c2c05c3fbd090939d4f0a6ae16290943ee1097
XX989tmx <issacgravity8@gmail.com>
2020-11-26 23:47:21 +0900

    adds basic structure 2

--- src/order/pages/Checkout.js
+++ src/order/pages/Checkout.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import reactBootstrap, { Col, Container, Row } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import reactBootstrap, { Col, Container, Row, Button } from "react-bootstrap";
 import { AuthContext } from "../../shared/context/auth-context";
 
 const Checkout = () => {
@@ -298,7 +298,73 @@ const Checkout = () => {
                   </Col>
                 </Row>
               </Col>
-              <Col lg={3}>side bar</Col>
+              <Col lg={3}>
+                <Row>
+                  <Col>
+                    <Row>
+                      <Col>
+                        <Link>
+                          {" "}
+                          <Button> 注文を確定する</Button>
+                        </Link>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Row>
+                          <Col>注文内容</Col>
+                        </Row>
+                        <Row>
+                          <Col>商品の小計</Col>
+                          <Col>total price</Col>
+                        </Row>
+                        <Row>
+                          <Col>配送料手数料</Col>
+                          <Col>cost</Col>
+                        </Row>
+                        <Row>
+                          <Col>御請求額</Col>
+                          <Col>total price</Col>
+                        </Row>
+                        <Row>
+                          <Col>獲得ポイント</Col>
+                          <Col>
+                            <Row>
+                              <Col xs={12}>acquirable points</Col>
+                              <Col xs={12}>円相当</Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Row>
+                      <Col>手数料配送料について</Col>
+                    </Row>
+                    <Row>
+                      <Col>tesuuryou expnanation</Col>
+                    </Row>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Row>
+                      <Col>
+                        <Link>
+                          {" "}
+                          <Button> 注文を確定する</Button>
+                        </Link>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>tyuumonn explanation</Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
             </Row>
             <Row>
               <Col>riyou kiyaku</Col>
