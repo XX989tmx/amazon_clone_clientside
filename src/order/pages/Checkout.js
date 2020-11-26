e1316144ed6b4f01d3ec8b09631c1715eaf72e8f
XX989tmx <issacgravity8@gmail.com>
2020-11-26 23:30:23 +0900

    adds basic structure to checkout js

--- src/order/pages/Checkout.js
+++ src/order/pages/Checkout.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import reactBootstrap, { Col, Container, Row } from "react-bootstrap";
 import { AuthContext } from "../../shared/context/auth-context";
 
 const Checkout = () => {
@@ -51,14 +52,260 @@ const Checkout = () => {
   };
   return (
     <div>
-      checkout
+      {/* checkout
       <form action="" onSubmit={completePurchaseHandler}>
         <select name="" id="paymentMethod" onChange={paymentMethodHandler}>
           <option value="">chose payment method</option>
           <option value="CreditCard">Credit Card</option>
         </select>
         <button type="submit">complete purchase</button>
-      </form>
+      </form> */}
+      <Container fluid="md">
+        <Row>
+          <Col xs={12}>
+            <Row>
+              <Col xs={12}>
+                <Row>
+                  <Col>bread crums image</Col>
+                </Row>
+                <Row>
+                  <Col>注文内容を確認変更する</Col>
+                </Row>
+                <Row>
+                  <Col>purchase confirmation paragraph</Col>
+                </Row>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={9}>
+                main section
+                <Row>
+                  <Col>notification section</Col>
+                </Row>
+                <Row>
+                  <Col xs={12} lg={4}>
+                    <Row>
+                      <Col>お届け先住所</Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Row>
+                          <Col>name</Col>
+                        </Row>
+                        <Row>
+                          <Col>zipcode</Col>
+                        </Row>
+                        <Row>
+                          <Col>addressinfo1</Col>
+                        </Row>
+                        <Row>
+                          <Col>addressinfo2</Col>
+                        </Row>
+                        <Row>
+                          <Col>phonenumber</Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col xs={12} lg={8}>
+                    <Row>
+                      <Col>
+                        <Row>
+                          <Col>
+                            <Row>
+                              <Col>支払い方法</Col>
+                            </Row>
+                            <Row>
+                              <Col>shimo4keta</Col>
+                            </Row>
+                            <Row>
+                              <Col>bunkatu explanation</Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            <Row>
+                              <Col>お支払い方法に紐付いた住所</Col>
+                            </Row>
+                            <Row>
+                              <Col>name</Col>
+                            </Row>
+                            <Row>
+                              <Col>zipcode</Col>
+                            </Row>
+                            <Row>
+                              <Col>addresinfo1</Col>
+                            </Row>
+                            <Row>
+                              <Col>addressinfo2</Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
+                      <Col>
+                        <Row>
+                          <Col>
+                            <Row>
+                              <Col>Amazon points</Col>
+                            </Row>
+                            <Row>
+                              <Col>point balance</Col>
+                            </Row>
+                            <Row>
+                              <Col>point use inout</Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    mainsection body section
+                    <Row>
+                      <Col>
+                        <Row>
+                          <Col>delivery date</Col>
+                          <Col>delivery date explanation</Col>
+                        </Row>
+                        <Row>
+                          <Col lg={6}>
+                            <Row>
+                              <Col xs={12} style={{ height: "200px" }}>
+                                <Row>
+                                  <Col xs={4}>
+                                    <Row>
+                                      <Col style={{ height: "200px" }}>img</Col>
+                                    </Row>
+                                  </Col>
+                                  <Col xs={8} style={{ height: "200px" }}>
+                                    <Row>
+                                      <Col>product name</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>price</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>gettable point</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>qantity</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>seller</Col>
+                                    </Row>
+                                  </Col>
+                                </Row>
+                              </Col>
+                              <Col xs={12} style={{ height: "200px" }}>
+                                <Row>
+                                  <Col xs={4}>
+                                    <Row>
+                                      <Col style={{ height: "200px" }}>img</Col>
+                                    </Row>
+                                  </Col>
+                                  <Col xs={8} style={{ height: "200px" }}>
+                                    <Row>
+                                      <Col>product name</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>price</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>gettable point</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>qantity</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>seller</Col>
+                                    </Row>
+                                  </Col>
+                                </Row>
+                              </Col>
+                              <Col xs={12} style={{ height: "200px" }}>
+                                <Row>
+                                  <Col xs={4}>
+                                    <Row>
+                                      <Col style={{ height: "200px" }}>img</Col>
+                                    </Row>
+                                  </Col>
+                                  <Col xs={8} style={{ height: "200px" }}>
+                                    <Row>
+                                      <Col>product name</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>price</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>gettable point</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>qantity</Col>
+                                    </Row>
+                                    <Row>
+                                      <Col>seller</Col>
+                                    </Row>
+                                  </Col>
+                                </Row>
+                              </Col>
+                            </Row>
+                          </Col>
+                          <Col lg={6}>
+                            <Row>
+                              <Col>プライム配送オプション</Col>
+                            </Row>
+                            <Row>
+                              <Col xs={1}></Col>
+                              <Col xs={11}>
+                                {" "}
+                                <Row>
+                                  <Col>tuuzyouhaisou explanation</Col>
+                                </Row>
+                                <Row>
+                                  <Col>manebarai</Col>
+                                </Row>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col xs={1}></Col>
+                              <Col xs={5}>oisogibinn</Col>
+                              <Col xs={5}>delivery date</Col>
+                            </Row>
+                            <Row>
+                              <Col xs={1}></Col>
+                              <Col xs={11}>
+                                <Row>
+                                  <Col>otodokenitizishiteibinn</Col>
+                                </Row>
+                                <Row>
+                                  <Col>delivery date</Col>
+                                </Row>
+                                <Row>
+                                  <Col>explanation</Col>
+                                </Row>
+                                <Row>
+                                  <Col> link to detail</Col>
+                                </Row>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+              <Col lg={3}>side bar</Col>
+            </Row>
+            <Row>
+              <Col>riyou kiyaku</Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
