9645c668f003168d38e64632ff7a44b94fffb8c5
XX989tmx <issacgravity8@gmail.com>
2020-11-29 23:30:58 +0900

    componentization 17

--- src/accounts/order-history/components/SideSectionBody.js
+++ src/accounts/order-history/components/SideSectionBody.js
@@ -1,51 +1,11 @@
 import React from "react";
 import reactBootstrap, { Row, Col, Container, Button } from "react-bootstrap";
+import BuyAgainItem from "./BuyAgainItem";
 const SideSectionBody = (props) => {
   return (
     <Row>
       <Col>
-        <Row>
-          <Col xs={12} md={12}>
-            <Row>
-              <Col style={{ height: "120px" }}>img</Col>
-            </Row>
-          </Col>
-          <Col xs={12} md={12}>
-            <Row>
-              <Col>name</Col>
-            </Row>
-            <Row>
-              <Col>price</Col>
-              <Col>icon</Col>
-            </Row>
-            <Row>
-              <Col>
-                <Button>カートに入れる</Button>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={12}>
-            <Row>
-              <Col style={{ height: "120px" }}>img</Col>
-            </Row>
-          </Col>
-          <Col xs={12} md={12}>
-            <Row>
-              <Col>name</Col>
-            </Row>
-            <Row>
-              <Col>price</Col>
-              <Col>icon</Col>
-            </Row>
-            <Row>
-              <Col>
-                <Button>カートに入れる</Button>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+        <BuyAgainItem />
       </Col>
     </Row>
   );
