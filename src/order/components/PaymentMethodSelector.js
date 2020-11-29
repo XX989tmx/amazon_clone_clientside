96c6d216d622c0157893ea0163d079379a5903e4
XX989tmx <issacgravity8@gmail.com>
2020-11-30 05:15:22 +0900

    adds payment method selector

--- src/order/components/PaymentMethodSelector.js
+++ src/order/components/PaymentMethodSelector.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import reactBootstrap, { Row, Col, Container, Button } from "react-bootstrap";
 const PaymentMethodSelector = (props) => {
+  const [paymentMethod, setPaymentMethod] = useState("creditCard");
+  const paymentMethodSelectorHandler = (event) => {
+    setPaymentMethod(event.target.value);
+    console.log(event.target.value);
+  };
   return (
     <Row>
-      <Col>payment selector</Col>
+      <Col>
+        <select value={paymentMethod} onChange={paymentMethodSelectorHandler}>
+          <option value="creditCard">クレジットカード</option>
+          <option value="debitCard">デビッドカード</option>
+        </select>
+      </Col>
     </Row>
   );
 };
