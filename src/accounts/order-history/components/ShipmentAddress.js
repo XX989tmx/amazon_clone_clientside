7e17c14b1cd6122d8d7c5b3d764c3c5d8b02a239
XX989tmx <issacgravity8@gmail.com>
2020-11-29 22:44:11 +0900

    componentization 8

---
+++ src/accounts/order-history/components/ShipmentAddress.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactBootstrap, { Row, Col, Container, Button } from "react-bootstrap";
+const ShipmentAddress = (props) => {
+  return (
+    <Row>
+      <Col>address name</Col>
+    </Row>
+  );
+};
+
+export default ShipmentAddress;
--- src/accounts/order-history/components/ShipmentAddressSection.js
+++ src/accounts/order-history/components/ShipmentAddressSection.js
@@ -1,14 +1,12 @@
 import React from "react";
 import reactBootstrap, { Row, Col, Container, Button } from "react-bootstrap";
+import ShipmentAddress from "./ShipmentAddress";
+import ShipmentAddressParagraph from "./ShipmentAddressParagraph";
 const ShipmentAddressSection = (props) => {
   return (
     <Col xs={6} lg={4}>
-      <Row>
-        <Col>お届け先</Col>
-      </Row>
-      <Row>
-        <Col>address name</Col>
-      </Row>
+      <ShipmentAddressParagraph />
+      <ShipmentAddress />
     </Col>
   );
 };
