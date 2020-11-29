a76aeb20f5992dc2867876717b8efef09117f9d0
XX989tmx <issacgravity8@gmail.com>
2020-11-29 23:03:50 +0900

    componentization 11

--- src/accounts/order-history/components/OrderedItems.js
+++ src/accounts/order-history/components/OrderedItems.js
@@ -1,52 +1,11 @@
 import React from "react";
 import reactBootstrap, { Row, Col, Container, Button } from "react-bootstrap";
+import OrderedItem from "./OrderedItem";
 const OrderedItems = (props) => {
   return (
     <Row>
       <Col>
-        <Row>
-          <Col md={2}>
-            <Row>
-              <Col style={{ height: "120px" }}>image</Col>
-            </Row>
-          </Col>
-          <Col md={10}>
-            <Row>
-              <Col>name</Col>
-            </Row>
-            <Row>
-              <Col>seller</Col>
-            </Row>
-            <Row>
-              <Col>price</Col>
-            </Row>
-            <Row>
-              <Col>buy again button</Col>
-            </Row>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col md={2}>
-            <Row>
-              <Col style={{ height: "120px" }}>image</Col>
-            </Row>
-          </Col>
-          <Col md={10}>
-            <Row>
-              <Col>name</Col>
-            </Row>
-            <Row>
-              <Col>seller</Col>
-            </Row>
-            <Row>
-              <Col>price</Col>
-            </Row>
-            <Row>
-              <Col>buy again button</Col>
-            </Row>
-          </Col>
-        </Row>
+        <OrderedItem />
       </Col>
     </Row>
   );
