0243b09b047a46ae9ac2173c4f23c61c5fce76d4
XX989tmx <issacgravity8@gmail.com>
2020-11-30 03:19:11 +0900

    componentization 5

--- src/order/components/AcquirablePointSection.js
+++ src/order/components/AcquirablePointSection.js
@@ -1,15 +1,11 @@
 import React from "react";
 import reactBootstrap, { Row, Col, Container, Button } from "react-bootstrap";
+import AcquirablePointsOfOrder from "./AcquirablePointsOfOrder";
 const AcquirablePointSection = (props) => {
   return (
     <Row>
       <Col>獲得ポイント</Col>
-      <Col>
-        <Row>
-          <Col xs={12}>acquirable points</Col>
-          <Col xs={12}>円相当</Col>
-        </Row>
-      </Col>
+      <AcquirablePointsOfOrder />
     </Row>
   );
 };
---
+++ src/order/components/AcquirablePointsOfOrder.js
@@ -0,0 +1,14 @@
+import React from "react";
+import reactBootstrap, { Row, Col, Container, Button } from "react-bootstrap";
+const AcquirablePointsOfOrder = (props) => {
+  return (
+    <Col>
+      <Row>
+        <Col xs={12}>acquirable points</Col>
+        <Col xs={12}>円相当</Col>
+      </Row>
+    </Col>
+  );
+};
+
+export default AcquirablePointsOfOrder;
