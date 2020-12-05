d786c5484de846fe99c8f4839e105b60ab3b8fbf
XX989tmx <issacgravity8@gmail.com>
2020-12-06 04:24:31 +0900

    componentization 4

--- src/shared/components/navigation/components/CartSection.js
+++ src/shared/components/navigation/components/CartSection.js
@@ -1,7 +1,13 @@
 import React from "react";
 import reactBootstrap, { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const CartSection = (props) => {
-  return <Col xs={3}>カート</Col>;
+  return (
+    <Col xs={3}>
+      {" "}
+      <Link to="/cart">カート</Link>
+    </Col>
+  );
 };
 
 export default CartSection;
