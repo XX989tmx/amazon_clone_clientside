041153cc9d35b9c2c08ba2957e7046a5fea53719
XX989tmx <issacgravity8@gmail.com>
2020-12-07 09:32:37 +0900

    adds basic layout to seller signup

--- src/sellers/pages/SellerSignup.js
+++ src/sellers/pages/SellerSignup.js
@@ -1,11 +1,13 @@
 import Axios from "axios";
 import React, { useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import reactBootstrap, {
   Alert,
   Button,
   Container,
   Form,
+  Row,
+  Col,
 } from "react-bootstrap";
 import { AuthContext } from "../../shared/context/auth-context";
 
@@ -66,33 +68,86 @@ const SellerSignup = () => {
   };
 
   return (
-    <Container>
-      <Alert variant={Message.variant}>{Message.message}</Alert>
+    <Container fluid="md">
+      <Row>
+        <Col xs={{ offset: 2, span: 8 }} lg={{ offset: 4, span: 4 }}>
+          logo
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Row>
+            <Col xs={{ offset: 2, span: 8 }} lg={{ offset: 4, span: 4 }}>
+              <Row>
+                <Col>
+                  <Alert variant={Message.variant}>{Message.message}</Alert>
+                </Col>
+              </Row>
+              <Row>
+                <Col>出品者アカウントを作成</Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form onSubmit={signupSubmitHandler}>
+                    <Form.Group
+                      controlId="name"
+                      value={Name}
+                      onChange={nameChangeHandler}
+                    >
+                      <Form.Label>お名前</Form.Label>
+                      <Form.Control type="text" placeholder="お名前" />
+                    </Form.Group>
+                    <Form.Group
+                      controlId="email"
+                      value={Email}
+                      onChange={emailChangeHandler}
+                    >
+                      <Form.Label>メールアドレス</Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="email@example.com"
+                      />
+                    </Form.Group>
+                    <Form.Group
+                      controlId="password"
+                      value={Password}
+                      onChange={passwordChangeHandler}
+                    >
+                      <Form.Label>パスワード</Form.Label>
+                      <Form.Control
+                        type="password"
+                        placeholder="半角英数字８文字以上"
+                      />
+                    </Form.Group>
 
-      <Form onSubmit={signupSubmitHandler}>
-        <Form.Group controlId="name" value={Name} onChange={nameChangeHandler}>
-          <Form.Label>お名前</Form.Label>
-          <Form.Control type="text" placeholder="お名前" />
-        </Form.Group>
-        <Form.Group
-          controlId="email"
-          value={Email}
-          onChange={emailChangeHandler}
-        >
-          <Form.Label>メールアドレス</Form.Label>
-          <Form.Control type="email" placeholder="email@example.com" />
-        </Form.Group>
-        <Form.Group
-          controlId="password"
-          value={Password}
-          onChange={passwordChangeHandler}
-        >
-          <Form.Label>パスワード</Form.Label>
-          <Form.Control type="password" placeholder="半角英数字８文字以上" />
-        </Form.Group>
-
-        <Button type="submit">出品者アカウントに登録</Button>
-      </Form>
+                    <Button type="submit">出品者アカウントを作成する</Button>
+                  </Form>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  ログインする事により、利用規約に同意したものとみなされます。
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  すでにアカウントをお持ちですか？<Link>　サインイン</Link>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={{ offset: 2, span: 8 }} lg={{ offset: 4, span: 4 }}>
+          <Row>
+            <Col>利用規約</Col>
+          </Row>
+          <Row>
+            <Col>company logo</Col>
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 };
