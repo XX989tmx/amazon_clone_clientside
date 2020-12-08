0fbe4124abb4df082736d2c131121213befdffe0
XX989tmx <issacgravity8@gmail.com>
2020-12-09 08:21:32 +0900

    componentization 35

--- src/shared/components/navigation/components/ItemOfPcAndDevice.js
+++ src/shared/components/navigation/components/ItemOfPcAndDevice.js
@@ -7,7 +7,20 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const ItemOfPcAndDevice = (props) => {
+  let itemOfPcAndDevice = [
+    "パソコン、タブレット",
+    "ディスプレイ、モニタ－",
+    "プリンター、インク",
+    "キーボード、マウス、入力機器",
+    "無線LAN、ネットワーク機器",
+    "PCパーツ、CPU、内蔵HDD",
+    "外付けドライブ、ストレージ",
+    "SSD,microSDカード、USBメモリ",
+    "PCアクセサリ、記録メディア",
+    "ゲーミングPC、関連アクセサリ",
+  ];
   return (
     <Row>
       <Col>
@@ -15,8 +28,18 @@ const ItemOfPcAndDevice = (props) => {
           <Col>パソコン、周辺機器</Col>
         </Row>
         <Row>
-          <Col xs={12}>すべてのパソコン、周辺機器</Col>
-          <Col xs={12}>each child category map</Col>
+          <Col xs={12}>
+            <Link
+              to={`/product/index/parentCategory/すべてのパソコン、周辺機器`}
+            >
+              すべてのパソコン、周辺機器
+            </Link>
+          </Col>
+          {itemOfPcAndDevice.map((v, i) => (
+            <Col xs={12} key={i}>
+              <Link to={`/product/index/childCategory/${v}`}>{v}</Link>
+            </Col>
+          ))}
         </Row>
       </Col>
     </Row>
