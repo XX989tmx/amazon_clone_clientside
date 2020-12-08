0c51ed9ccf01eef888c29ac7ae26f709ee2ab65d
XX989tmx <issacgravity8@gmail.com>
2020-12-09 08:11:08 +0900

    componentization 33

--- src/shared/components/navigation/components/ItemOfHomeElectronicsAndCamera.js
+++ src/shared/components/navigation/components/ItemOfHomeElectronicsAndCamera.js
@@ -7,7 +7,25 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const ItemOfHomeElectronicsAndCamera = (props) => {
+  let itemOfHomeElectronicsAndCamera = [
+    "キッチン家電",
+    "生活家電",
+    "理美容家電",
+    "空調、季節家電",
+    "照明",
+    "大型家電",
+    "カメラ、ビデオカメラ",
+    "アクションカメラ",
+    "双眼鏡、望遠鏡、光学機器",
+    "携帯電話、スマートフォン",
+    "テレビ、レコーダー",
+    "オーディオ",
+    "イヤホン、ヘッドホン",
+    "ウェアラブルデバイス",
+    "アクセサリ",
+  ];
   return (
     <Row>
       <Col>
@@ -15,8 +33,21 @@ const ItemOfHomeElectronicsAndCamera = (props) => {
           <Col>家電、カメラ、AV機器</Col>
         </Row>
         <Row>
-          <Col xs={12}>すべての家電</Col>
-          <Col xs={12}>each child category map</Col>
+          <Col xs={12}>
+            <Link to={`/product/index/parentCategory/すべての家電`}>
+              すべての家電
+            </Link>
+          </Col>
+          <Col xs={12}>
+            <Link to={`/product/index/parentCategory/すべてのカメラ、AV機器`}>
+              すべてのカメラ、AV機器
+            </Link>
+          </Col>
+          {itemOfHomeElectronicsAndCamera.map((v, i) => (
+            <Col xs={12} key={i}>
+              <Link to={`/product/index/childCategory/${v}`}>{v}</Link>
+            </Col>
+          ))}
         </Row>
       </Col>
     </Row>
