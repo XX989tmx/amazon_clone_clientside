842035f2c6dc711ee9a0b223d6e1c79d03d22d13
XX989tmx <issacgravity8@gmail.com>
2020-12-08 09:15:15 +0900

    adds category structure 3

--- src/shared/components/navigation/SideNavigation.js
+++ src/shared/components/navigation/SideNavigation.js
@@ -153,7 +153,38 @@ const SideNavigation = (props) => {
                       </Col>
                     </Row>
                     <Row>
-                      <Col>家電、カメラ、AV機器</Col>
+                      <Col>
+                        <Row>
+                          <Col>家電、カメラ、AV機器 click</Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            show close
+                            <Row>
+                              <Col>
+                                <Row>
+                                  <Col>家電、カメラ、AV機器</Col>
+                                </Row>
+                                <Row>
+                                  <Col xs={12}>すべての家電</Col>
+                                  <Col xs={12}>each child category map</Col>
+                                </Row>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col>
+                                <Row>
+                                  <Col>中古ストア</Col>
+                                </Row>
+                                <Row>
+                                  <Col xs={12}>すべての中古</Col>
+                                  <Col xs={12}>each child category map</Col>
+                                </Row>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
                     </Row>
                     <Row>
                       <Col>パソコン、オフィス用品</Col>
