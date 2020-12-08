e555e08efac47c8bf5c9f2ee4544b069083e19ab
XX989tmx <issacgravity8@gmail.com>
2020-12-09 06:12:09 +0900

    componentization 8

--- src/shared/components/navigation/components/HomeKitchenPet.js
+++ src/shared/components/navigation/components/HomeKitchenPet.js
@@ -7,6 +7,7 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import HomeKitchenPetCategoryItems from "./HomeKitchenPetCategoryItems";
 const HomeKitchenPet = (props) => {
   return (
     <Row>
@@ -14,43 +15,7 @@ const HomeKitchenPet = (props) => {
         <Row>
           <Col>ホーム＆キッチン、ペット、DIY click</Col>
         </Row>
-        <Row>
-          <Col>
-            show close
-            <Row>
-              <Col>
-                <Row>
-                  <Col>ホーム＆キッチン</Col>
-                </Row>
-                <Row>
-                  <Col xs={12}>すべてのホーム＆キッチン</Col>
-                  <Col xs={12}>each child category map</Col>
-                </Row>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Row>
-                  <Col>DIY、工具、ガーデン</Col>
-                </Row>
-                <Row>
-                  <Col xs={12}>すべてのDIY、工具、ガーデン</Col>
-                  <Col xs={12}>each child category map</Col>
-                </Row>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Row>
-                  <Col>ペット</Col>
-                </Row>
-                <Row>
-                  <Col xs={12}>each child category map</Col>
-                </Row>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+        <HomeKitchenPetCategoryItems />
       </Col>
     </Row>
   );
---
+++ src/shared/components/navigation/components/HomeKitchenPetCategoryItems.js
@@ -0,0 +1,52 @@
+import React from "react";
+import reactBootstrap, {
+  Container,
+  Alert,
+  Button,
+  Form,
+  Row,
+  Col,
+} from "react-bootstrap";
+const HomeKitchenPetCategoryItems = (props) => {
+  return (
+    <Row>
+      <Col>
+        show close
+        <Row>
+          <Col>
+            <Row>
+              <Col>ホーム＆キッチン</Col>
+            </Row>
+            <Row>
+              <Col xs={12}>すべてのホーム＆キッチン</Col>
+              <Col xs={12}>each child category map</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Row>
+              <Col>DIY、工具、ガーデン</Col>
+            </Row>
+            <Row>
+              <Col xs={12}>すべてのDIY、工具、ガーデン</Col>
+              <Col xs={12}>each child category map</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Row>
+              <Col>ペット</Col>
+            </Row>
+            <Row>
+              <Col xs={12}>each child category map</Col>
+            </Row>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default HomeKitchenPetCategoryItems;
