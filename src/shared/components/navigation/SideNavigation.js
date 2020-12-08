9b25ba39e2507e6616c8eb036209b2f14188f49f
XX989tmx <issacgravity8@gmail.com>
2020-12-08 09:36:40 +0900

    adds category structure 8

--- src/shared/components/navigation/SideNavigation.js
+++ src/shared/components/navigation/SideNavigation.js
@@ -344,7 +344,26 @@ const SideNavigation = (props) => {
                       </Col>
                     </Row>
                     <Row>
-                      <Col>ベビー、おもちゃ、ホビー</Col>
+                      <Col>
+                        <Row>
+                          <Col>ベビー、おもちゃ、ホビー click</Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            show close
+                            <Row>
+                              <Col>
+                                <Row>
+                                  <Col>ベビー、おもちゃ、ホビー</Col>
+                                </Row>
+                                <Row>
+                                  <Col xs={12}>each child category map</Col>
+                                </Row>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
                     </Row>
                     <Row>
                       <Col>服、シューズ、バッグ、腕時計</Col>
