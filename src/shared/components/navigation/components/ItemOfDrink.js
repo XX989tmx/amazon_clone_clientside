ee95276ae8dbd63c3a3ca5bf98a59e6952e9085d
XX989tmx <issacgravity8@gmail.com>
2020-12-10 14:39:56 +0900

    componentization 42

--- src/shared/components/navigation/components/ItemOfDrink.js
+++ src/shared/components/navigation/components/ItemOfDrink.js
@@ -7,7 +7,20 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const ItemOfDrink = (props) => {
+  let itemOfDrink = [
+    "ビール、発泡酒",
+    "ワイン",
+    "日本酒",
+    "焼酎",
+    "梅酒",
+    "洋酒、リキュール",
+    "チューハイ、カクテル",
+    "ノンアルコール飲料",
+    "winery direct(直輸入ワイン)",
+    "Amazonソムリエ",
+  ];
   return (
     <Row>
       <Col>
@@ -15,8 +28,16 @@ const ItemOfDrink = (props) => {
           <Col>お酒</Col>
         </Row>
         <Row>
-          <Col xs={12}>すべてのお酒</Col>
-          <Col xs={12}>each child category map</Col>
+          <Col xs={12}>
+            <Link to={`/product/index/parentCategory/すべてのお酒`}>
+              すべてのお酒
+            </Link>
+          </Col>
+          {itemOfDrink.map((v, i) => (
+            <Col xs={12} key={i}>
+              <Link to={`/product/index/childCategory/${v}`}>{v}</Link>
+            </Col>
+          ))}
         </Row>
       </Col>
     </Row>
