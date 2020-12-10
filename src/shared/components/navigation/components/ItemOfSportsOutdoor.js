0e0d0783fc9b3b7bf48762cee4cf67b6b0574a9f
XX989tmx <issacgravity8@gmail.com>
2020-12-10 14:54:33 +0900

    componentization 47

--- src/shared/components/navigation/components/ItemOfSportsOutdoor.js
+++ src/shared/components/navigation/components/ItemOfSportsOutdoor.js
@@ -7,7 +7,16 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const ItemOfSportsOutdoor = (props) => {
+  let itemOfSportsOutdoor = [
+    "自転車",
+    "アウトドア",
+    "釣り",
+    "フィットネス、トレーニング",
+    "ゴルフ",
+    "スポーツウェア＆シューズ",
+  ];
   return (
     <Row>
       <Col>
@@ -15,8 +24,18 @@ const ItemOfSportsOutdoor = (props) => {
           <Col>スポーツ＆アウトドア</Col>
         </Row>
         <Row>
-          <Col xs={12}>すべてのスポーツ＆アウトドア</Col>
-          <Col xs={12}>each child category map</Col>
+          <Col xs={12}>
+            <Link
+              to={`/product/index/parentCategory/すべてのスポーツ＆アウトドア`}
+            >
+              すべてのスポーツ＆アウトドア
+            </Link>
+          </Col>
+          {itemOfSportsOutdoor.map((v, i) => (
+            <Col xs={12} key={i}>
+              <Link to={`/product/index/childCategory/${v}`}>{v}</Link>
+            </Col>
+          ))}
         </Row>
       </Col>
     </Row>
