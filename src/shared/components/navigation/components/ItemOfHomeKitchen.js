b9387eb0a25392964d861c871888846c1034189c
XX989tmx <issacgravity8@gmail.com>
2020-12-10 14:27:27 +0900

    componentization 38

--- src/shared/components/navigation/components/ItemOfHomeKitchen.js
+++ src/shared/components/navigation/components/ItemOfHomeKitchen.js
@@ -7,7 +7,21 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const ItemOfHomeKitchen = (props) => {
+  let itemOfHomeKitchen = [
+    "キッチン用品、食器",
+    "インテリア、雑貨",
+    "カーペット、カーテン、クッション",
+    "家具",
+    "収納用品、収納家具",
+    "布団、枕、シーツ",
+    "掃除、洗濯、バス、トイレ",
+    "防犯、防災",
+    "家電",
+    "手芸、画材",
+    "ショールーム",
+  ];
   return (
     <Row>
       <Col>
@@ -15,8 +29,16 @@ const ItemOfHomeKitchen = (props) => {
           <Col>ホーム＆キッチン</Col>
         </Row>
         <Row>
-          <Col xs={12}>すべてのホーム＆キッチン</Col>
-          <Col xs={12}>each child category map</Col>
+          <Col xs={12}>
+            <Link to={`/product/index/parentCategory/すべてのホーム＆キッチン`}>
+              すべてのホーム＆キッチン
+            </Link>
+          </Col>
+          {itemOfHomeKitchen.map((v, i) => (
+            <Col xs={12} key={i}>
+              <Link to={`/product/index/childCategory/${v}`}>{v}</Link>
+            </Col>
+          ))}
         </Row>
       </Col>
     </Row>
