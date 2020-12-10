25e1e62758798f2bc492d1972ed99cd6383aef87
XX989tmx <issacgravity8@gmail.com>
2020-12-10 15:25:43 +0900

    adds state 9

--- src/shared/components/navigation/components/ClothShoesBag.js
+++ src/shared/components/navigation/components/ClothShoesBag.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import reactBootstrap, {
   Container,
   Alert,
@@ -8,14 +8,23 @@ import reactBootstrap, {
   Col,
 } from "react-bootstrap";
 import ClothShoesBagCategoryItems from "./ClothShoesBagCategoryItems";
+
 const ClothShoesBag = (props) => {
+  const [isCategoryCardOpen, setIsCategoryCardOpen] = useState(false);
+
+  const categoryCardOpener = () => {
+    setIsCategoryCardOpen((prev) => !prev);
+  };
+
   return (
     <Row>
       <Col>
         <Row>
-          <Col>服、シューズ、バッグ、腕時計 click</Col>
+          <Col onClick={categoryCardOpener}>
+            服、シューズ、バッグ、腕時計 {isCategoryCardOpen ? "close" : "open"}
+          </Col>
         </Row>
-        <ClothShoesBagCategoryItems />
+        {isCategoryCardOpen && <ClothShoesBagCategoryItems />}
       </Col>
     </Row>
   );
