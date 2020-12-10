a5ddf5ffadb786c7c159e95f8323221132bd80e3
XX989tmx <issacgravity8@gmail.com>
2020-12-10 20:15:48 +0900

    adds link

--- src/accounts/overview/components/LowerRow.js
+++ src/accounts/overview/components/LowerRow.js
@@ -7,38 +7,45 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const LowerRow = (props) => {
   return (
     <Row>
       <Col xs={12} md={6} lg={4}>
-        <Row>
-          <Col xs={2} md={3}>
-            img
-          </Col>
-          <Col xs={10} md={9}>
-            <Row>
-              <Col>アドレス帳</Col>
-            </Row>
-            <Row>
-              <Col>登録住所の追加変更</Col>
-            </Row>
-          </Col>
-        </Row>
+        <Link to={`/account/addresses`}>
+          {" "}
+          <Row>
+            <Col xs={2} md={3}>
+              img
+            </Col>
+            <Col xs={10} md={9}>
+              <Row>
+                <Col>アドレス帳</Col>
+              </Row>
+              <Row>
+                <Col>登録住所の追加変更</Col>
+              </Row>
+            </Col>
+          </Row>
+        </Link>
       </Col>
       <Col xs={12} md={6} lg={4}>
-        <Row>
-          <Col xs={2} md={3}>
-            img
-          </Col>
-          <Col xs={10} md={9}>
-            <Row>
-              <Col>お客様の支払い方法</Col>
-            </Row>
-            <Row>
-              <Col>支払い方法と設定の管理、残高とオファーの表示</Col>
-            </Row>
-          </Col>
-        </Row>
+        <Link to={`/account/your-payments/payment-methods`}>
+          {" "}
+          <Row>
+            <Col xs={2} md={3}>
+              img
+            </Col>
+            <Col xs={10} md={9}>
+              <Row>
+                <Col>お客様の支払い方法</Col>
+              </Row>
+              <Row>
+                <Col>支払い方法と設定の管理、残高とオファーの表示</Col>
+              </Row>
+            </Col>
+          </Row>
+        </Link>
       </Col>
       <Col xs={12} md={6} lg={4}>
         <Row>
--- src/accounts/overview/components/UpperRow.js
+++ src/accounts/overview/components/UpperRow.js
@@ -7,38 +7,45 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const UpperRow = (props) => {
   return (
     <Row>
       <Col xs={12} md={6} lg={4}>
-        <Row>
-          <Col xs={2} md={3}>
-            img
-          </Col>
-          <Col xs={10} md={9}>
-            <Row>
-              <Col>注文履歴</Col>
-            </Row>
-            <Row>
-              <Col>配送状況の確認返品手続き</Col>
-            </Row>
-          </Col>
-        </Row>
+        <Link to={`/account/order-history`}>
+          {" "}
+          <Row>
+            <Col xs={2} md={3}>
+              img
+            </Col>
+            <Col xs={10} md={9}>
+              <Row>
+                <Col>注文履歴</Col>
+              </Row>
+              <Row>
+                <Col>配送状況の確認返品手続き</Col>
+              </Row>
+            </Col>
+          </Row>
+        </Link>
       </Col>
       <Col xs={12} md={6} lg={4}>
-        <Row>
-          <Col xs={2} md={3}>
-            img
-          </Col>
-          <Col xs={10} md={9}>
-            <Row>
-              <Col>ログインとセキュリティ</Col>
-            </Row>
-            <Row>
-              <Col>アカウント情報の変更</Col>
-            </Row>
-          </Col>
-        </Row>
+        <Link>
+          {" "}
+          <Row>
+            <Col xs={2} md={3}>
+              img
+            </Col>
+            <Col xs={10} md={9}>
+              <Row>
+                <Col>ログインとセキュリティ</Col>
+              </Row>
+              <Row>
+                <Col>アカウント情報の変更</Col>
+              </Row>
+            </Col>
+          </Row>
+        </Link>
       </Col>
       <Col xs={12} md={6} lg={4}>
         <Row>
