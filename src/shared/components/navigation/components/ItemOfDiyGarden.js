82a06f1cea407433adbf87dc89a90f2375bad042
XX989tmx <issacgravity8@gmail.com>
2020-12-10 14:30:30 +0900

    componentization 39

--- src/shared/components/navigation/components/ItemOfDiyGarden.js
+++ src/shared/components/navigation/components/ItemOfDiyGarden.js
@@ -7,7 +7,15 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const ItemOfDiyGarden = (props) => {
+  let itemOfDiyGarden = [
+    "電動工具",
+    "作業工具",
+    "リフォーム",
+    "ガーデン",
+    "エクステリア",
+  ];
   return (
     <Row>
       <Col>
@@ -15,8 +23,18 @@ const ItemOfDiyGarden = (props) => {
           <Col>DIY、工具、ガーデン</Col>
         </Row>
         <Row>
-          <Col xs={12}>すべてのDIY、工具、ガーデン</Col>
-          <Col xs={12}>each child category map</Col>
+          <Col xs={12}>
+            <Link
+              to={`/product/index/parentCategory/すべてのDIY、工具、ガーデン`}
+            >
+              すべてのDIY、工具、ガーデン
+            </Link>
+          </Col>
+          {itemOfDiyGarden.map((v, i) => (
+            <Col xs={12} key={i}>
+              <Link to={`/product/index/childCategory/${v}`}>{v}</Link>
+            </Col>
+          ))}
         </Row>
       </Col>
     </Row>
