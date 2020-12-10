0b7c0b753f14ace16f50d8966f224636517492ab
XX989tmx <issacgravity8@gmail.com>
2020-12-10 14:43:14 +0900

    componentization 43

--- src/shared/components/navigation/components/ItemOfDrugStore.js
+++ src/shared/components/navigation/components/ItemOfDrugStore.js
@@ -7,7 +7,24 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const ItemOfDrugStore = (props) => {
+  let itemOfDrugStore = [
+    "医薬品",
+    "ヘルスケア、衛生用品",
+    "コンタクトレンズ",
+    "サプリメント",
+    "ダイエット",
+    "シニアサポート、介護",
+    "おむつ、おしりふき",
+    "日用品（掃除、洗濯、キッチン）",
+    "ドラッグストアへ",
+    "Amazonブランド",
+    "バーチャルダッシュ",
+    "Amazon Dash Replinishment",
+    "Amazon パントリー",
+    "Amazon定期おトク便",
+  ];
   return (
     <Row>
       <Col>
@@ -15,7 +32,11 @@ const ItemOfDrugStore = (props) => {
           <Col>ドラッグストア</Col>
         </Row>
         <Row>
-          <Col xs={12}>each child category map</Col>
+          {itemOfDrugStore.map((v, i) => (
+            <Col xs={12} key={i}>
+              <Link to={`/product/index/childCategory/${v}`}>{v}</Link>
+            </Col>
+          ))}
         </Row>
       </Col>
     </Row>
