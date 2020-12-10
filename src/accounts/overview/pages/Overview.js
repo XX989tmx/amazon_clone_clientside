75ff96ecdee94f8a16d771939373a189f76bbc48
XX989tmx <issacgravity8@gmail.com>
2020-12-10 20:00:10 +0900

    adds structure to overview 2

--- src/accounts/overview/pages/Overview.js
+++ src/accounts/overview/pages/Overview.js
@@ -121,9 +121,51 @@ const Overview = () => {
                   </Col>
                 </Row>
                 <Row>
-                  <Col>
+                  <Col xs={12} md={6} lg={4}>
                     <Row>
-                      <Col></Col>
+                      <Col>
+                        <Row>
+                          <Col>お買い物設定</Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            <Row>
+                              <Col>非表示にした注文の表示</Col>
+                            </Row>
+                            <Row>
+                              <Col>言語設定の変更</Col>
+                            </Row>
+                            <Row>
+                              <Col>マイリスト</Col>
+                            </Row>
+                            <Row>
+                              <Col>プロフィール</Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col xs={12} md={6} lg={4}>
+                    <Row>
+                      <Col>
+                        <Row>
+                          <Col>その他アカウント</Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            <Row>
+                              <Col>出品サービスアカウント</Col>
+                            </Row>
+                            <Row>
+                              <Col>出品用アカウントにログイン</Col>
+                            </Row>
+                            <Row>
+                              <Col>Amazonログインの設定</Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
                     </Row>
                   </Col>
                 </Row>
