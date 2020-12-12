b281627a83d6ee5c57e5999fa4c6f0a1c22299bf
XX989tmx <issacgravity8@gmail.com>
2020-12-12 15:07:37 +0900

    componentization 2

---
+++ src/products/components/ImageInputSection.js
@@ -0,0 +1,76 @@
+import React from "react";
+import reactBootstrap, {
+  Container,
+  Alert,
+  Button,
+  Form,
+  Row,
+  Col,
+} from "react-bootstrap";
+const ImageInputSection = (props) => {
+  return (
+    <Row>
+      <Col>
+        <Row>
+          <Col>
+            <Row>
+              <Col>title</Col>
+            </Row>
+            <Row>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>action</Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col>title</Col>
+            </Row>
+            <Row>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>action</Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col>title</Col>
+            </Row>
+            <Row>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>action</Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col>title</Col>
+            </Row>
+            <Row>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>action</Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col>title</Col>
+            </Row>
+            <Row>
+              <Col>image</Col>
+            </Row>
+            <Row>
+              <Col>action</Col>
+            </Row>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default ImageInputSection;
