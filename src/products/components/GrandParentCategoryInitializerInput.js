b172559941d65fdab02b33c0a663e9070b5308ec
XX989tmx <issacgravity8@gmail.com>
2020-12-12 15:26:39 +0900

    componentization 3

---
+++ src/products/components/GrandParentCategoryInitializerInput.js
@@ -0,0 +1,25 @@
+import React from "react";
+import reactBootstrap, {
+  Container,
+  Alert,
+  Button,
+  Form,
+  Row,
+  Col,
+} from "react-bootstrap";
+const GrandParentCategoryInitializerInput = (props) => {
+  return (
+    <Row>
+      <Col>
+        <Row>
+          <Col>label</Col>
+        </Row>
+        <Row>
+          <Col>input</Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default GrandParentCategoryInitializerInput;
--- src/products/components/TextInputSection.js
+++ src/products/components/TextInputSection.js
@@ -7,131 +7,35 @@ import reactBootstrap, {
   Row,
   Col,
 } from "react-bootstrap";
+import BrandInput from "./BrandInput";
+import ChildCategoryInput from "./ChildCategoryInput";
+import DeliveryDateInput from "./DeliveryDateInput";
+import GrandChildCategoryInput from "./GrandChildCategoryInput";
+import GrandGrandChildCategoryInput from "./GrandGrandChildCategoryInput";
+import GrandParentCategoryInitializerInput from "./GrandParentCategoryInitializerInput";
+import GrandParentCategoryInput from "./GrandParentCategoryInput";
+import IsStockInput from "./IsStockInput";
+import NameInput from "./NameInput";
+import ParentCategoryInput from "./ParentCategoryInput";
+import PriceInput from "./PriceInput";
+import StockQuantityInput from "./StockQuantityInput";
 const TextInputSection = (props) => {
   return (
     <Row>
       <Col>
         text
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>label</Col>
-            </Row>
-            <Row>
-              <Col>input</Col>
-            </Row>
-          </Col>
-        </Row>
+        <NameInput />
+        <PriceInput />
+        <DeliveryDateInput />
+        <BrandInput />
+        <StockQuantityInput />
+        <IsStockInput />
+        <GrandParentCategoryInitializerInput />
+        <GrandParentCategoryInput />
+        <ParentCategoryInput />
+        <ChildCategoryInput />
+        <GrandChildCategoryInput />
+        <GrandGrandChildCategoryInput />
         <Row>
           <Col>
             <Row>
